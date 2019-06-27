819a920c35005fdb9aca3129151267e01d38f6af
Gylling, Randy <Randy.Gylling@rexelusa.com>
2019-06-27 13:44:20 -0700

    stubbing out basically server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+
+const port = 8000
+
+app.listen(port, () => { console.log('We are live on ' + port) })
