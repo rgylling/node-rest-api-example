8b1596b58ac5054c7f81f1cc8c9ed0871e6bfc36
Gylling, Randy <Randy.Gylling@rexelusa.com>
2019-06-27 14:00:57 -0700

    parse it

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ const MongoClient = require('mongodb').MongoClient
 const app = express()
 
 const port = 8000
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+require('./app/routes')(app, {})
 app.listen(port, () => { console.log('We are live on ' + port) })
